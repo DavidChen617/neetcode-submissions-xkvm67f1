4fdadb5c02691ec3830374726c62cc837f58d4d2
David Chen <162773720+DavidChen617@users.noreply.github.com>
2026-04-12 16:59:37 +0800

    Add: interleaving-string - submission-6

---
+++ Data Structures & Algorithms/interleaving-string/submission-6.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool IsInterleave(string s1, string s2, string s3) {
+        if(s1.Length + s2.Length != s3.Length)
+            return false;
+            
+        var dp = new bool?[s1.Length + 1, s2.Length + 1];
+
+        return DFS(0, 0, 0);
+
+        bool DFS(int i, int j, int k){
+
+            if(k == s3.Length){
+                return s1.Length == i && s2.Length == j;
+            }
+
+            if(dp[i, j].HasValue)
+                return dp[i, j].Value;
+
+            var res = false;
+
+            if(i < s1.Length && s1[i] == s3[k])
+                res = DFS(i + 1, j, k + 1);
+
+            if(!res && j < s2.Length && s2[j] == s3[k])
+                res = DFS(i, j + 1, k + 1);
+
+            dp[i, j] = res;
+
+            return res;
+        }
+    }
+}
